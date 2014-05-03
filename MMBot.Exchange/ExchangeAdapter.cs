5948cb7fdf86a412e78259f0c1f46d8950540b78
David Sosby <david@sosby.org>
2014-05-03 10:14:08 -0700

    Initial stub of project

---
+++ MMBot.Exchange/ExchangeAdapter.cs
@@ -0,0 +1,42 @@
+﻿using Common.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMBot.Exchange
+{
+    public class ExchangeAdapter : Adapter
+    {
+        public ExchangeAdapter(ILog logger, string adapterId)
+            : base(logger, adapterId)
+        {
+        }
+
+        public override void Initialize(Robot robot)
+        {
+            base.Initialize(robot);
+            Configure();
+        }
+
+        private void Configure()
+        {
+            //TODO: Configuration
+        }
+
+        public override Task Close()
+        {
+            Logger.Info("Closing Exchange adapter connection");
+            throw new NotImplementedException();
+        }
+
+        public override Task Run()
+        {
+            Logger.Info("Starting Exchange adapter connection");
+            throw new NotImplementedException();
+        }
+
+        //TODO: Send, Reply, Emote
+    }
+}
