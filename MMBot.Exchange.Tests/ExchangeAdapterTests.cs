7e60ea1fb9e7473eaa8bad9ed98560598acc2e96
David Sosby <david@sosby.org>
2014-05-23 10:00:09 -0700

    Adding some unit test setup

--- MMBot.Exchange.Tests/ExchangeAdapterTests.cs
+++ MMBot.Exchange.Tests/ExchangeAdapterTests.cs
@@ -6,11 +6,23 @@ namespace MMBot.Exchange.Tests
     [TestClass]
     public class ExchangeAdapterTests
     {
+        MMBot.Exchange.ExchangeAdapter adapter = new MMBot.Exchange.ExchangeAdapter(null, "");
+
+        public ExchangeAdapterTests()
+        {
+            adapter = new MMBot.Exchange.ExchangeAdapter(null, "");
+        }
+
         [TestMethod]
         public void CanInitializeWithNullRobot()
         {
-            var adapter = new MMBot.Exchange.ExchangeAdapter(null, "");
             adapter.Initialize(null);
+        }
+
+        [TestMethod]
+        public void ShouldSendStartupEmail()
+        {
+            adapter.Run();
         }
     }
 }
